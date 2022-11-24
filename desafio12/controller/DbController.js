c38fe263c8a9eb771c1d856b78c8e972fb22eaa4
AgustinaBallesteros <ballesterosagustina@hotmail.com>
2022-11-24 13:45:52 -0300

    Log-in por formulario

---
+++ desafio12/controller/DbController.js
@@ -0,0 +1,49 @@
+import knex from 'knex';
+
+async function createTable(options ,name, schema) {
+    const knexInstance = knex(options);
+
+    const exist = await knexInstance.schema.hasTable(name);
+    if (exist) {
+        console.log(`La tabla ${name} ya existe`);
+        return;
+    }
+
+    try {
+        await knexInstance.schema.createTable(name, schema);
+        console.log(`Tabla ${name} creada`); 
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    } finally {
+        knexInstance.destroy();
+    }
+}
+
+async function selectData(options, database) {
+    const knexInstance = knex(options);
+    try {
+        const data = await knexInstance.from(database).select('*');
+        return data;
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    } finally {
+        knexInstance.destroy();
+    }
+}
+
+async function insertData(options, database, data) {
+    const knexInstance = knex(options);
+    try {
+        await knexInstance(database).insert(data);
+        console.log(`Datos insertados en la tabla ${database}`);
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    } finally {
+        knexInstance.destroy();
+    }
+}
+
+export { createTable, selectData , insertData };
---
+++ desafio12/controller/ProductosController.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+
+class ProductosController {
+    constructor(productos) {
+        this.productos = productos;
+    }
+    listar() {
+        return this.productos;
+    }
+    listarPorId(id) {
+        return this.productos.filter((producto) => producto.id == id);
+    }
+    guardar(producto) {
+        this.productos.push(producto);
+        console.log('productos--------', this.productos);
+        fs.writeFileSync(path.join(__dirname, '../model/arrProductos.json'), JSON.stringify(this.productos, null, '\t'));
+        
+    }
+    actualizar(id, producto) {
+        this.productos = this.productos.map((producto) => {
+            if (producto.id == id) {
+                producto = producto;
+            }
+            return producto;
+        });
+    }
+    borrar(id) {
+        this.productos = this.productos.filter((producto) => producto.id != id);
+    }
+
+}
+
+async function createTable(name, schema) {
+    const knexInstance = knex(options);
+
+    const exist = await knexInstance.schema.hasTable(name);
+    if (exist) {
+        console.log(`La tabla ${name} ya existe`);
+        return;
+    }
+
+    try {
+        await knexInstance.schema.createTable(name, schema);
+        console.log(`Tabla ${name} creada`); 
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    } finally {
+        knexInstance.destroy();
+    }
+}
+
+export { ProductosController, createTable };
+
